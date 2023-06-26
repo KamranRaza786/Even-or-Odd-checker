29fa9c9dfde58920ef80ed5043da874d4c53c217
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-06-26 18:52:23 +0500

    edit

---
+++ script.mjs
@@ -0,0 +1,16 @@
+
+    function evenAndodd (event) {
+        event.preventDefault();
+        let userInput = +document.querySelector(`#userInput`).value;
+        if (userInput % 2 == 0) { 
+            document.querySelector(`#result`).innerHTML = "The Number is Even";
+        }else{
+            document.querySelector(`#result`).innerHTML = "The Number is Odd";
+        }
+    }
+    function resetInput(){
+        document.querySelector(`#userInput`).value = '';
+        document.querySelector(`#result`).innerHTML = '';
+    }
+    document.addEventListener(`submit`, evenAndodd);
+    document.querySelector(`#resetButton`).addEventListener('click', resetInput);
